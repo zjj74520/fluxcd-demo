b667f6fb2c22971d2f44b985eaee31098f7ff541
zjj74520 <49038544+zjj74520@users.noreply.github.com>
2023-03-07 17:09:15 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,9 +23,16 @@ terraform {
 
 provider "kubernetes" {
   config_path   = "/etc/kubernetes/admin.conf"
+  host                   = azurerm_kubernetes_cluster.aks.kube_admin_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_admin_config.0.cluster_ca_certificate)
 }
 resource "kubernetes_namespace" "nginx-test" {
   metadata {
     name = "nginx"
   }
 }
+
+
+

cc4265d66253171247051abafa29424bed206969
zjj74520 <49038544+zjj74520@users.noreply.github.com>
2023-03-08 20:32:32 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,53 +11,7 @@ resource "kubernetes_namespace" "test" {
     name = "mynginx1"
   }
 }
-resource "kubernetes_deployment" "test" {
-  metadata {
-    name      = "nginx"
-    namespace = kubernetes_namespace.test.metadata[0].name
-  }
-  spec {
-    replicas = 2
-    selector {
-      match_labels = {
-        app = "MyTestApp"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          app = "MyTestApp"
-        }
-      }
-      spec {
-        container {
-          image = "nginx"
-          name  = "nginx-container123132"
-          port {
-            container_port = 80
-          }
-        }
-      }
-    }
-  }
-}
-resource "kubernetes_service" "test" {
-  metadata {
-    name      = "nginx"
-    namespace = kubernetes_namespace.test.metadata[0].name
-  }
-  spec {
-    selector = {
-      app = kubernetes_deployment.test.spec[0].template[0].metadata[0].labels.app
-    }
-    type = "NodePort"
-    port {
-      node_port   = 30201
-      port        = 80
-      target_port = 80
-    }
-  }
-}
+
 
 
 
